94038df7f274e89b742b07f747b07c7778b47210
Osman Ekrem Korkmaz <korkmazosmanekrem@gmail.com>
2022-07-11 13:21:15 +0300

    some changes

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,12 @@
-self.addEventListener('install', e => console.log('pwa installed.'));
-self.addEventListener('fetch', event => {}); // şimdilik bir şey yapmaya gerek yok
+let cacheData = "AppV1";
+this.addEventListener('install', e => console.log('pwa installed.'));
+
+this.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if(response) {
+                return response;
+            }
+        })
+    );
+});
